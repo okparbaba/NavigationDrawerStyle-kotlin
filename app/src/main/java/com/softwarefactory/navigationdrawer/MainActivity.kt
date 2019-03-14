010213432f1d222d71c8677521d475fcef6f4462
Yannaing Lynn <rghtbossy@gmail.com>
2019-03-14 14:03:16 +0630

    this version can be used

---
+++ app/src/main/java/com/softwarefactory/navigationdrawer/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.softwarefactory.navigationdrawer
+
+import android.graphics.Color
+import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val toggle= object :ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close){
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+                super.onDrawerSlide(drawerView, slideOffset)
+                val slideX = drawerView.width*slideOffset
+                //Slide
+                coordinator_layout.translationX = slideX
+                //Next style
+                //coordinator_layout.scaleX = 1-(slideOffset/5f)
+                //coordinator_layout.scaleY = 1-(slideOffset/5f)
+
+                //Next Next Style
+                //coordinator_layout.scaleX = 1-slideOffset
+                //coordinator_layout.scaleY = 1-slideOffset
+
+
+            }
+        }
+        drawer_layout.addDrawerListener(toggle)
+        drawer_layout.setScrimColor(Color.TRANSPARENT)
+        nav_view.setNavigationItemSelectedListener(this)
+
+        img_drawer_menu.setOnClickListener {
+            if (drawer_layout.isDrawerOpen(GravityCompat.START))
+                drawer_layout.closeDrawer(GravityCompat.START)
+            else
+                drawer_layout.openDrawer(GravityCompat.START)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_camera -> {
+                // Handle the camera action
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_manage -> {
+
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
